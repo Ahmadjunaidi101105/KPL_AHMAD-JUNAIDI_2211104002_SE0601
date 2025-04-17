4435fe73d7856cfeb9e3291501e9b5b578082bb1
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-04-17 12:03:34 +0700

    mengumpulkan TP 09

---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpmodul9_2211104002
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
---
+++ 09_API_Design_dan_Construction_Using_Swagger/TP/MahasiswaController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using modul9_2211104002;
+using tpmodul9_2211104002;
+
+namespace tpmodul9_2211104026.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa { Nama = "Ahmad Junaidi", Nim = "2211104002" },
+            new Mahasiswa { Nama = "Kholiahdina", Nim = "2211104006" },
+            new Mahasiswa { Nama = "Nita fitrotul", Nim = "2211104023" },
+            new Mahasiswa { Nama = "Alfian Mutakim", Nim = "2211104011" }
+        };
+
+        // GET /api/mahasiswa
+        [HttpGet]
+        public ActionResult<List<Mahasiswa>> GetAll()
+        {
+            return daftarMahasiswa;
+        }
+
+        // GET /api/mahasiswa/{index}
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetByIndex(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+
+            return daftarMahasiswa[index];
+        }
+
+        // POST /api/mahasiswa
+        [HttpPost]
+        public ActionResult AddMahasiswa(Mahasiswa mhs)
+        {
+            daftarMahasiswa.Add(mhs);
+            return Ok();
+        }
+
+        // DELETE /api/mahasiswa/{index}
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+                return NotFound();
+
+            daftarMahasiswa.RemoveAt(index);
+            return Ok();
+        }
+    }
+}

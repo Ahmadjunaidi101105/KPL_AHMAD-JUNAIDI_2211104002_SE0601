9e11b2d29861bfa017ae0dad683e49914fe80441
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-04-18 15:08:33 +0700

    Mengumpulkan TP_10 Library Construction

---
+++ 10_Library_Construction/TP/tp10_2211104002/tp10_2211104002/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using AljabarLibraries;
+
+namespace TPConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var akar = Aljabar.AkarPersamaanKuadrat(new double[] { 1, -3, -10 });
+            Console.WriteLine($"Akar: {akar[0]}, {akar[1]}");
+
+            var hasilKuadrat = Aljabar.HasilKuadrat(new double[] { 2, -3 });
+            Console.WriteLine($"Hasil Kuadrat: {hasilKuadrat[0]}x^2 + {hasilKuadrat[1]}x + {hasilKuadrat[2]}");
+        }
+    }
+}

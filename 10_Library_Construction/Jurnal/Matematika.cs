f2832270825e0ff8c9d383811310b728dfa48125
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-01 14:08:34 +0700

    mengunpulkan jurnal

---
+++ 10_Library_Construction/Jurnal/Matematika.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace MatematikaLibraries
+{
+    public class Matematika
+    {
+        public int FPB(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+
+        public int KPK(int a, int b)
+        {
+            return (a * b) / FPB(a, b);
+        }
+
+        public string Turunan(int[] persamaan)
+        {
+            string hasil = "";
+            int pangkat = persamaan.Length - 1;
+            for (int i = 0; i < persamaan.Length - 1; i++)
+            {
+                int koefisien = persamaan[i] * (pangkat - i);
+                int derajatBaru = pangkat - i - 1;
+
+                if (koefisien == 0) continue;
+
+                if (hasil.Length > 0 && koefisien > 0)
+                    hasil += " + ";
+
+                if (koefisien < 0)
+                    hasil += " - " + Math.Abs(koefisien);
+                else
+                    hasil += koefisien;
+
+                if (derajatBaru > 1)
+                    hasil += "x" + derajatBaru;
+                else if (derajatBaru == 1)
+                    hasil += "x";
+            }
+            return hasil.Trim();
+        }
+
+        public string Integral(int[] persamaan)
+        {
+            string hasil = "";
+            int pangkat = persamaan.Length;
+
+            for (int i = 0; i < persamaan.Length; i++)
+            {
+                int derajatBaru = pangkat - i;
+                double koefisienBaru = (double)persamaan[i] / derajatBaru;
+
+                if (i != 0 && koefisienBaru > 0)
+                    hasil += " + ";
+                else if (koefisienBaru < 0)
+                    hasil += " - ";
+
+                hasil += Math.Abs(koefisienBaru);
+
+                if (derajatBaru > 1)
+                    hasil += "x" + derajatBaru;
+                else
+                    hasil += "x";
+            }
+
+            hasil += " + C";
+            return hasil;
+        }
+    }
+}
---
+++ 10_Library_Construction/Jurnal/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using MatematikaLibraries;
+
+namespace modul10_NIM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Matematika mtk = new Matematika();
+
+            Console.WriteLine("FPB dari 60 dan 45: " + mtk.FPB(60, 45));
+            Console.WriteLine("KPK dari 12 dan 8: " + mtk.KPK(12, 8));
+            Console.WriteLine("Turunan dari x^3 + 4x^2 -12x + 9: " + mtk.Turunan(new int[] { 1, 4, -12, 9 }));
+            Console.WriteLine("Integral dari 4x^3 + 6x^2 -12x + 9: " + mtk.Integral(new int[] { 4, 6, -12, 9 }));
+
+            Console.ReadKey();
+        }
+    }
+}

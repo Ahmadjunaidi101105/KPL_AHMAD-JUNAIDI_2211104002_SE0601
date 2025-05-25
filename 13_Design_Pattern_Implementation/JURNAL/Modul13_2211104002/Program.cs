5ab1e2bdd7c7d59e428db072ca6e2bcb8e7d164a
“Ahmad <ahmadjunaidibeds@gmail.com>
2025-05-25 15:37:57 +0700

    Mengumpulka Jurnal modul 13

---
+++ 13_Design_Pattern_Implementation/JURNAL/Modul13_2211104002/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace modul13_1202210001
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // A. Buat dua variabel singleton
+            PusatDataSingleton data1 = PusatDataSingleton.GetDataSingleton();
+            PusatDataSingleton data2 = PusatDataSingleton.GetDataSingleton();
+
+            // B. Tambahkan data nama anggota & asisten praktikum ke data1
+            data1.AddSebuahData("Ahmad Junaidi");
+            data1.AddSebuahData("Rafli Dhafin Kamil");
+            data1.AddSebuahData("Alfian Mutakim");
+            data1.AddSebuahData("Muhammad Lutfi Hamdani");
+            data1.AddSebuahData("Asisten: Budi");
+
+            // C. Print semua data dari data2
+            data2.PrintSemuaData();
+
+            // D. Hapus asisten dari data2
+            data2.HapusSebuahData(4); // menghapus "Asisten: Budi"
+
+            // E. Print ulang dari data1 untuk memastikan data sudah dihapus
+            data1.PrintSemuaData();
+
+            // F. Print jumlah elemen dari data1 dan data2
+            Console.WriteLine("Jumlah data pada data1: " + data1.GetSemuaData().Count);
+            Console.WriteLine("Jumlah data pada data2: " + data2.GetSemuaData().Count);
+        }
+    }
+}
---
+++ 13_Design_Pattern_Implementation/JURNAL/Modul13_2211104002/PusatDataSingleton.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace modul13_1202210001
+{
+    public class PusatDataSingleton
+    {
+        private static PusatDataSingleton _instance;
+        public List<string> DataTersimpan { get; set; }
+
+        // Konstruktor private
+        private PusatDataSingleton()
+        {
+            DataTersimpan = new List<string>();
+        }
+
+        // Method singleton
+        public static PusatDataSingleton GetDataSingleton()
+        {
+            if (_instance == null)
+            {
+                _instance = new PusatDataSingleton();
+            }
+            return _instance;
+        }
+
+        public List<string> GetSemuaData()
+        {
+            return DataTersimpan;
+        }
+
+        public void PrintSemuaData()
+        {
+            Console.WriteLine("Data saat ini:");
+            for (int i = 0; i < DataTersimpan.Count; i++)
+            {
+                Console.WriteLine($"{i}. {DataTersimpan[i]}");
+            }
+        }
+
+        public void AddSebuahData(string input)
+        {
+            DataTersimpan.Add(input);
+        }
+
+        public void HapusSebuahData(int index)
+        {
+            if (index >= 0 && index < DataTersimpan.Count)
+            {
+                DataTersimpan.RemoveAt(index);
+            }
+            else
+            {
+                Console.WriteLine("Index tidak valid.");
+            }
+        }
+    }
+}
